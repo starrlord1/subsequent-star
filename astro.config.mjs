25f9289a0e2f7c040a49f6a5ad4cdd5591415fbb
techmurda0000 <techmurda@protonmail.com>
2024-06-28 15:06:11 -0500

    Changes to astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+    output: 'static'
+
+
+
+{
+    site: 'https://www.techmurda.org'
+  }
